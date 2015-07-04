cbd82f6dcd6870365842b0452e68060552fd638a
AlphaHinex <AlphaHinex@gmail.com>
2015-07-04 12:17:11 +0800

    Mkdirs of parent

--- src/test/groovy/org/hinex/alpha/arsenal/test/TestResource.groovy
+++ src/test/groovy/org/hinex/alpha/arsenal/test/TestResource.groovy
@@ -3,7 +3,9 @@ package org.hinex.alpha.arsenal.test
 class TestResource {
     
     static File getFile(String fileName) {
-        new File("${new File('').getAbsolutePath()}/src/test/resources/$fileName")
+        def file = new File("${new File('').getAbsolutePath()}/src/test/resources/$fileName")
+        file.getParentFile().mkdirs()
+        file
     }
 
 }
